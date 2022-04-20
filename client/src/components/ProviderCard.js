86b81e3babad3848ce45d1028793d94242ac0664
Brian Diaz <bidiaz101@gmail.com>
2022-04-20 09:43:10 -0500

    rendered user info

--- client/src/components/ProviderCard.js
+++ client/src/components/ProviderCard.js
@@ -1,18 +1,44 @@
 import React, { useState, useEffect} from 'react'
 
 function ProviderCard({ provider }) {
-    const [comic, setComic] = useState('')
+    const [comic, setComic] = useState({
+        img: '',
+        title: ''
+    })
 
     useEffect(() => {
         fetch('/comics/random')
           .then(resp => resp.json())
-          .then(data => console.log(data))
+          .then(data => {
+              const { title, img } = data
+              setComic({
+                  title: title,
+                  img: img
+              })
+          })
     }, [])
 
     const { picture, name, gender, cell, email, location} = provider
 
+    function formatName(nameObj){
+        const {title, first, last} = nameObj
+        return `${title}. ${first} ${last}`
+    }
+
+    function formatLocation(locationObj){
+        const { street, city, state, country, postcode } = locationObj
+        return `${street.number} ${street.name}, ${city}, ${state}, ${postcode}, ${country}`
+    }
+
     return (
-        <h1>ProviderCard</h1>
+        <div>
+            <img src={picture.medium} alt={formatName(name)} />
+            <h2>{formatName(name)}</h2>
+            <p>{gender}</p>
+            <p>{cell}</p>
+            <p>{email}</p>
+            <p>{formatLocation(location)}</p>
+        </div>
     )
 }
 
